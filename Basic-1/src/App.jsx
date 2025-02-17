65e9f66d77ed8c415ae960d2ea4ac867e5b04575
Priyanshu2229140 <priyanshujana07@gmail.com>
2025-02-17 09:32:58 +0530

    Props

---
+++ Basic-1/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Contact from "./Contact";
+
+function App() {
+    return (
+        <div className="contacts">
+            <Contact 
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <Contact      
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <Contact 
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact 
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
+        </div>
+    );
+}
+
+export default App;
