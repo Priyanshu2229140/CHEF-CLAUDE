1b45b2aa81117f6829e284ff129763dd9a218697
Priyanshu2229140 <priyanshujana07@gmail.com>
2025-03-22 12:14:41 +0530

    Added text before form

--- Chef-Claude/src/App.jsx
+++ Chef-Claude/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Header from './header'
 import Main from './main_part'
+import Text from './text'
 function App() {
 
   return (
     <>
       <Header />
+      <Text />
       <Main />
+      
     </>
   )
 }
---
+++ Chef-Claude/src/text.jsx
@@ -0,0 +1,7 @@
+export default function Text(){
+    return(
+        <p className="text_in_between">
+            What do you want to cook today ?&#128522;
+        </p>
+    );
+}   
